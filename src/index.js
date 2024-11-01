91875f972275bd07a65d2ed2a01f1d299cbd72a0
sm-armstrong <sapphiramk@gmail.com>
2024-11-01 14:43:22 +0000

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generateQuote(event) {
     let context = "You are a Quotation expert and love to share inspiring and thought-provoking quotes. Your task is to generate a short quote, with appropriate citation, using basic HTML. The citation should be in a <strong> element. Make sure to follow the user instructions. Please behave.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let quoteElement = document.querySelector("#quote");
+    quoteElement.classList.remove("hidden");
+    quoteElement.innerHTML = `<div class="generating">⏲️ Generating a quote about ${instructionsInput.value}...</div>`;
+
     console.log("Generating quote")
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
